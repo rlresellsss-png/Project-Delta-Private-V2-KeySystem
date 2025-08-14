49620a703fde879519cd655e53aca9be6302aa86
rlresellsss-png <rl.resellsss@gmail.com>
2025-08-14 06:16:16 -0400

    Update PD Private Key System V2.lua

--- PD Private Key System V2.lua
+++ PD Private Key System V2.lua
@@ -1,14 +1,14 @@
-local KeyGuardLibrary =
-    loadstring(game:HttpGet('https://cdn.keyguardian.org/library/v1.0.0.lua'))()
-local trueData = '63bde6292e674b0b8567a2520a58d484'
-local falseData = '147cd9e8051a4b0cb97a7f0e08c2b533'
+local KeyGuardLibrary = loadstring(game:HttpGet("https://cdn.keyguardian.org/library/v1.0.0.lua"))()
+local trueData = "eb7ba93e7c13440c81c413ffb95e0d44"
+local falseData = "1fe4d7a023ec4a75af1d80590fe6cc5c"
 
 KeyGuardLibrary.Set({
-    publicToken = 'a9389132077740de915b96812d00be3c',
-    privateToken = '641d5eef305f45cf9af2f91c53b36f85',
-    trueData = trueData,
-    falseData = falseData,
+	publicToken = "a9389132077740de915b96812d00be3c",
+	privateToken = "641d5eef305f45cf9af2f91c53b36f85",
+	trueData = trueData,
+	falseData = falseData,
 })
+
 local getkey = KeyGuardLibrary.getLink()
 
 local response = KeyGuardLibrary.validatePremiumKey(key)
@@ -23,11 +23,6 @@ else
     game.StarterGui:SetCore('SendNotification', {
         Title = 'Key Failed!',
         Text = 'Please Check Key And Try Again',
-        Time = 3,
+        Time = 4,
     })
 end
-
-
-
-
-
