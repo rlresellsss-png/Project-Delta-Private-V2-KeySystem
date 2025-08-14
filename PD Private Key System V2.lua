e9173faf835999cbdc428a741787763416afaff3
rlresellsss-png <rl.resellsss@gmail.com>
2025-08-14 05:52:28 -0400

    Update PD Private Key System V2.lua

--- PD Private Key System V2.lua
+++ PD Private Key System V2.lua
@@ -1,7 +1,7 @@
 local KeyGuardLibrary =
     loadstring(game:HttpGet('https://cdn.keyguardian.org/library/v1.0.0.lua'))()
-local trueData = '8497af9d4b25486596b5cf86a5752b76'
-local falseData = '8a61aea15feb4604b4f01069e4572e03'
+local trueData = 'eed840f486df48b0b63de21c545b08ee'
+local falseData = '025b09a19b7e4b818f32b187690a3003'
 
 KeyGuardLibrary.Set({
     publicToken = 'a9389132077740de915b96812d00be3c',
@@ -10,75 +10,20 @@ KeyGuardLibrary.Set({
     falseData = falseData,
 })
 
-local Fluent = loadstring(
-    game:HttpGet(
-        'https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua'
-    )
-)()
-local Key = ''
+local getkey = KeyGuardLibrary.getLink()
 
-local Window = Fluent:CreateWindow({
-    Title = 'Project Delta Private Script - Key System',
-    SubTitle = 'by - breakneckv09',
-    TabWidth = 160,
-    Size = UDim2.fromOffset(490, 270),
-    Acrylic = false,
-    Theme = 'Dark',
-    MinimizeKey = Enum.KeyCode.LeftControl,
-})
+local response = KeyGuardLibrary.validatePremiumKey(Private_Key)
 
-local Tabs = {
-    KeySys = Window:AddTab({
-        Title = 'Project Delta Private | V2',
-        Icon = 'key',
-    }),
-}
-
-local Entkey = Tabs.KeySys:AddInput('Input', {
-    Title = 'Enter Key',
-    Description = '',
-    Default = '',
-    Placeholder = 'Enter key…',
-    Numeric = false,
-    Finished = false,
-    Callback = function(Value)
-        Key = Value
-    end,
-})
-
-local Checkkey = Tabs.KeySys:AddButton({
-    Title = 'Check Key',
-    Description = '',
-    Callback = function()
-        local response = KeyGuardLibrary.validatePremiumKey(Key)
-        if response == trueData then
-            loadstring(
-                game:HttpGet(
-                    'https://raw.githubusercontent.com/slotsuntiliwin/Project-Delta-Private-V2/refs/heads/main/Project%20Delta%20Private%20V2.lua'
-                )
-            )()
-        else
-            game.StarterGui:SetCore('SendNotification', {
-                Title = 'Key Failed',
-                Text = 'Please Check Key And Try Again',
-                Time = 4,
-            })
-        end
-    end,
-})
-
-local Getkey = Tabs.KeySys:AddButton({
-    Title = 'Copy Store Link',
-    Description = '',
-    Callback = function()
-        game.StarterGui:SetCore('SendNotification', {
-            Title = 'Copied Link!',
-            Text = '',
-            Time = 3,
-        })
-
-        setclipboard('https://bandzhubsupply.mysellauth.com')
-    end,
-})
-
-Window:SelectTab(1)
+if response == trueData then
+    loadstring(
+        game:HttpGet(
+            'https://raw.githubusercontent.com/slotsuntiliwin/Project-Delta-Private-V2/refs/heads/main/Project%20Delta%20Private%20V2.lua'
+        )
+    )()
+else
+    game.StarterGui:SetCore('SendNotification', {
+        Title = 'Key Failed!',
+        Text = 'Please Check Key And Try Again',
+        Time = 3,
+    })
+end
